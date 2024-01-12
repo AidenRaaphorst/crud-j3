19d5034d095ecf82f1097d6f9f95fb708749f428
marcosmug <94381046+marcosmug@users.noreply.github.com>
2024-01-12 15:05:04 +0100

    Nav bar :)

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,23 +15,45 @@
         @vite('resources/css/app.css')
     </head>
     <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
+        <nav class="bg-[#00ff00] h-32 flex flex-row justify-between items-center">
+            <img class="h-full" src="/img/header-target.png" alt="Header image">
 
-            <p class="bg-amber-400">test</p>
+            <div class="p-4 w-full flex flex-row justify-evenly font-medium text-white-600 dark:text-white-500">
+                <a href="#">Discord</a>
+                <a href="#">Forums</a>
+                <a href="#">About</a>
+                <a href="#">Merch</a>
+            </div>
 
+            <div class="p-4 gap-4 flex flex-row justify-evenly">
+                <p>TIETTER</p>
+            </div>
+        </nav>
+
+        <div class="container mt-20">
+            <p class="bg-[#675F5F] text-white text-3xl">Latest</p>
+            <p class="bg-[#675F5F] text-white text-3xl">Community records</p>
         </div>
+
+{{--        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">--}}
+{{--            @if (Route::has('login'))--}}
+{{--                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">--}}
+{{--                    @auth--}}
+{{--                        <a href="{{ url('/dashboard') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Dashboard</a>--}}
+{{--                    @else--}}
+{{--                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>--}}
+
+{{--                        @if (Route::has('register'))--}}
+{{--                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>--}}
+{{--                        @endif--}}
+{{--                    @endauth--}}
+{{--                </div>--}}
+{{--            @endif--}}
+
+
+{{--            <p class="bg-amber-400">test</p>--}}
+
+{{--        </div>--}}
     </body>
 </html>
